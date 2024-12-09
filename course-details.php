b9711fd3ccafdaee7ef37188781eea757980bda8
shobit3 <shobit@edtechinnovate.com>
2024-12-09 17:23:03 -0500

    latest updated changes

--- course-details.php
+++ course-details.php
@@ -136,59 +136,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="single-course-sidebar">
-                        <div class="course-widget">
-                            <div class="course-video">
-                                <a href="https://www.youtube.com/watch?v=PWvPbGWVRrU" class="popup-youtube play-now ">
-                                    <i class="ri-play-fill cr-video-btn"></i>
-                                    <span class="ripple"></span>
-                                </a>
-                            </div>
-                            <div class="sidebar-content">
-                                <div class="meta-info d-flex align-items-center justify-content-between mb-20">
-                                    <h3>$400</h3>
-                                    <div class="share">
-                                        <img src="assets/img/icon/share-icon.svg" alt="icon">
-                                    </div>
-                                </div>
-                                <h4 class="fs-20 mb-20">This Course Includes:</h4>
-                                <ul class="courses-details">
-                                    <li>
-                                        <div class="icon"><img src="assets/img/icon/time-icon.svg" alt="icon"> Course Duration</div>
-                                        <p>2 Hour 10 Min</p>
-                                    </li>
-                                    <li>
-                                        <div class="icon"><img src="assets/img/icon/menu-icon.svg" alt="icon"> Category</div>
-                                        <p>Graphics Designer</p>
-                                    </li>
-                                    <li>
-                                        <div class="icon"><img src="assets/img/icon/lavel-icon.svg" alt="icon"> Course Level</div>
-                                        <p>Higher</p>
-                                    </li>
-                                    <li>
-                                        <div class="icon"><img src="assets/img/icon/heat-icon.svg" alt="icon"> Student Enrolled</div>
-                                        <p>50</p>
-                                    </li>
-                                    <li>
-                                        <div class="icon"><img src="assets/img/icon/update-icon.svg" alt="icon"> Last Update</div>
-                                        <p>Dec 12-20-2023</p>
-                                    </li>
-                                    <li>
-                                        <div class="icon"><img src="assets/img/icon/skill-icon.svg" alt="icon"> Skill Level</div>
-                                        <p>Beginner</p>
-                                    </li>
-                                    <li>
-                                        <div class="icon"><img src="assets/img/icon/global-icon-sm.svg" alt="icon"> Language</div>
-                                        <p>English</p>
-                                    </li>
-                                </ul>
-                                <a href="contact.html" class="btn style-one box-shadow-1 mb-20">Buy This Course <img src="assets/img/icon/long-arrow.svg" alt="icon"></a>
-                                <a href="cart.html" class="btn style-one dec-clor">Add to Wishlist</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
             <div class="related-course pt-70">
                 <div class="course-section-title mb-30 text-center">
@@ -197,129 +145,56 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-4 col-sm-6">
-                        <div class="single-courses-box mb-25 box-shadow-2">
+                        <div class="p-3 mb-25 box-shadow-2">
                             <div class="image mb-20 position-relative">
                                 <a href="course-details.html" class="d-block">
                                     <img src="assets/img/all-img/course-1.png" alt="image">
                                 </a>
-                                <div class="cr-option">
-                                    <a href="author.html"><i class="ri-heart-fill"></i></a>
-                                    <a href="author.html"><i class="ri-shopping-basket-fill"></i></a>
-                                </div>
-                                <div class="cr-tag">
-                                    <a href="course-details.html">Engineering</a>
-                                </div>
+                               
                             </div>
                             <div class="content">
                                 <div class="meta-info mb-20 d-flex align-items-center justify-content-between">
-                                    <div class="author d-flex align-items-center">
-                                        <img src="assets/img/all-img/user-1.png" alt="image">
-                                        <span>Eleanor Pena</span>
-                                    </div>
-                                    <div class="cr-price">
-                                        <h5 class="fs-16"><span class="price">$100/</span> <span class="old-price">$400</span></h5>
-                                    </div>
+                                    
+                                    
                                 </div>
                                 <h3 class="mb-15 fs-20"><a href="blog-details.php">Full web Designing Course with 42 web Template.</a></h3>
-                                <div class="ratings mb-20">
-                                    <ul class="d-flex list-unstyle p-0">
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><span>(45)</span></li>
-                                    </ul>
-                                </div>
+                               
                             </div>
-                            <ul class="cr-items d-flex list-unstyle">
-                                <li class="mr-15"><i class="ri-vidicon-fill"></i> <span>(45) lesson</span> </li>
-                                <li><i class="ri-time-line"></i> <span>10 Week</span></li>
-                            </ul>
+                           
                         </div>
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <div class="single-courses-box mb-25 box-shadow-2">
+                        <div class="p-3 mb-25 box-shadow-2">
                             <div class="image mb-20 position-relative">
                                 <a href="course-details.html" class="d-block">
                                     <img src="assets/img/all-img/course-3.png" alt="image">
                                 </a>
-                                <div class="cr-option">
-                                    <a href="author.html"><i class="ri-heart-fill"></i></a>
-                                    <a href="author.html"><i class="ri-shopping-basket-fill"></i></a>
-                                </div>
-                                <div class="cr-tag">
-                                    <a href="course-details.html">Engineering</a>
-                                </div>
+                               
+                               
                             </div>
                             <div class="content">
-                                <div class="meta-info mb-20 d-flex align-items-center justify-content-between">
-                                    <div class="author d-flex align-items-center">
-                                        <img src="assets/img/all-img/user-3.png" alt="image">
-                                        <span>Jacob Jones</span>
-                                    </div>
-                                    <div class="cr-price">
-                                        <h5 class="fs-16"><span class="price">$100/</span> <span class="old-price">$400</span></h5>
-                                    </div>
-                                </div>
+                                
                                 <h3 class="mb-15 fs-20"><a href="blog-details.php">Full web Designing Course with 42 web Template.</a></h3>
-                                <div class="ratings mb-20">
-                                    <ul class="d-flex list-unstyle p-0">
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><span>(45)</span></li>
-                                    </ul>
-                                </div>
+                                
                             </div>
-                            <ul class="cr-items d-flex list-unstyle">
-                                <li class="mr-15"><i class="ri-vidicon-fill"></i> <span>(45) lesson</span> </li>
-                                <li><i class="ri-time-line"></i> <span>10 Week</span></li>
-                            </ul>
+                            
                         </div>
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <div class="single-courses-box mb-25 box-shadow-2">
+                        <div class="p-3 mb-25 box-shadow-2">
                             <div class="image mb-20 position-relative">
                                 <a href="course-details.html" class="d-block">
                                     <img src="assets/img/all-img/course-4.png" alt="image">
                                 </a>
-                                <div class="cr-option">
-                                    <a href="author.html"><i class="ri-heart-fill"></i></a>
-                                    <a href="author.html"><i class="ri-shopping-basket-fill"></i></a>
-                                </div>
-                                <div class="cr-tag">
-                                    <a href="course-details.html">Engineering</a>
-                                </div>
+                                
+                                
                             </div>
                             <div class="content">
-                                <div class="meta-info mb-20 d-flex align-items-center justify-content-between">
-                                    <div class="author d-flex align-items-center">
-                                        <img src="assets/img/all-img/user-4.png" alt="image">
-                                        <span>Esther Howard</span>
-                                    </div>
-                                    <div class="cr-price">
-                                        <h5 class="fs-16"><span class="price">$100/</span> <span class="old-price">$400</span></h5>
-                                    </div>
-                                </div>
+                                
                                 <h3 class="mb-15 fs-20"><a href="blog-details.php">Full web Designing Course with 42 web Template.</a></h3>
-                                <div class="ratings mb-20">
-                                    <ul class="d-flex list-unstyle p-0">
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><i class="ri-star-fill"></i></li>
-                                        <li><span>(45)</span></li>
-                                    </ul>
-                                </div>
+                                
                             </div>
-                            <ul class="cr-items d-flex list-unstyle">
-                                <li class="mr-15"><i class="ri-vidicon-fill"></i> <span>(45) lesson</span> </li>
-                                <li><i class="ri-time-line"></i> <span>10 Week</span></li>
-                            </ul>
+                            
                         </div>
                     </div>
                 </div>
